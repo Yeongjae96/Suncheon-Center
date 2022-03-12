e03142b8a8456b4b70aa4823a57bb5a142e7a0e6
Yeongjae96 <67811731+Yeongjae96@users.noreply.github.com>
2022-03-12 18:23:40 +0900

    init frontend
    init frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    "generator-star-spacing": "off",
+    indent: ["error", 2],
+    semi: 0,
+    quotes: ["off", "single"],
+    "no-console": "off",
+    "comma-dangle": 0,
+    // "comma-dangle": [
+    //   "error",
+    //   {
+    //     arrays: "ignore",
+    //     objects: "ignore",
+    //     imports: "ignore",
+    //     exports: "ignore",
+    //     functions: "ignore"
+    //   },
+    // ],
+    "space-in-parens": ["error", "never"],
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "space-before-blocks": ["error", "always"],
+    "no-empty": "error",
+    "no-duplicate-imports": "error",
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["default"],
+      },
+    ],
+    "eol-last": 0,
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+    "no-unused-vars": "warn"
+  },
+};
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Hi
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
